7da8522535af555848c760e852d9e1ad5c12b72a
saravanakumar <saravanasunrises@gmail.com>
2025-11-22 13:51:12 +0530

    Spring Boor

---
+++ src/main/java/com/example/MySpringProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringProjectApplication.class, args);
+    }
+}
